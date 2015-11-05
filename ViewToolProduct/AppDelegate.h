845810ad9cc04c5faddbd0cccdeeb3302538bdf0
lisheng_xu <xls_man@foxmail.com>
2015-11-05 09:21:54 +0800

    Initial Commit

---
+++ ViewToolProduct/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViewToolProduct
+//
+//  Created by 一卡易 on 15/11/5.
+//  Copyright © 2015年 1card1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewToolProduct/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ViewToolProduct
+//
+//  Created by 一卡易 on 15/11/5.
+//  Copyright © 2015年 1card1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
